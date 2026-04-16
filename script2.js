199db53270d1275a8d205774f9ae5fe8f74ba752
Void-Coffee <Mireona.Demon@gmail.com>
2026-04-16 12:27:16 +0200

    Delete script2.js

--- script2.js
+++
@@ -1,9 +0,0 @@
-console.log("SMC page loaded");
-
-// Simple fade-in animation
-document.body.style.opacity = 0;
-
-window.onload = () => {
-    document.body.style.transition = "1s";
-    document.body.style.opacity = 1;
-};
